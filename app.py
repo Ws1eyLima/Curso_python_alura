e52bb84c5da297aebd875056ef2f692c389d6897
Weslley Caires <caireswsl5@gmail.com>
2025-01-02 21:05:15 -0300

    Firt commit, python alura

---
+++ app.py
@@ -0,0 +1,15 @@
+print("""
+    Sabor Express
+    -------------------------
+    [1] Cadastrar Restaurante
+    [2] Listar Restaurante
+    [3] Ativar Restaurante
+    [0] Sair
+    """)
+
+opcao_escolhida = input("Escolha uma opção: ")
+
+print(f"Voce escolheu a opção: {opcao_escolhida}")
+
+PI = 3.14159
+print(f"{PI:.2f}")
